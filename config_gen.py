02da1e06a676ea343ff2ee76ac1dc3449232a554
Martijn Courteaux <courteauxmartijn@gmail.com>
2016-08-20 02:16:25 +0200

    Added support for the -iquote flag.
    I didn't investigate the code too much, but adding `"-iquote"` in that array seemed to do the trick for me.

--- config_gen.py
+++ config_gen.py
@@ -326,7 +326,7 @@ def parse_flags(build_log):
     define_regex = re.compile("-D([a-zA-Z0-9_]+)=(.*)")
 
     # Used to only bundle filenames with applicable arguments
-    filename_flags = ["-o", "-I", "-isystem", "-include", "-imacros", "-isysroot"]
+    filename_flags = ["-o", "-I", "-isystem", "-iquote", "-include", "-imacros", "-isysroot"]
 
     # Process build log
     for line in build_log:
